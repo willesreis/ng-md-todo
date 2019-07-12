33516fc00842e85fb431c8f7c9227d90f73896b0
Willes Reis <willesreis@hotmail.com>
2019-07-12 02:57:21 -0300

    improved task list

--- src/main/groovy/org/wsr/ngmdtodo/services/TaskService.groovy
+++ src/main/groovy/org/wsr/ngmdtodo/services/TaskService.groovy
@@ -3,8 +3,10 @@ package org.wsr.ngmdtodo.services
 import org.wsr.ngmdtodo.domain.Task
 import org.wsr.ngmdtodo.sql.SqlStatements
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PostMapping
 
 @RestController
 class TaskService {
@@ -15,20 +17,29 @@ class TaskService {
     @GetMapping("/create")
     void create() {
         statement.executeWithTransaction(
-            "CREATE TABLE IF NOT EXISTS Task (id INTEGER NOT NULL IDENTITY, checked BOOL NULL, description VARCHAR(100) NULL)",
-            "INSERT INTO Task (checked, description) VALUES (false, 'Teste unchecked')",
-            "INSERT INTO Task (checked, description) VALUES (true, 'Teste checked')"
+            "CREATE TABLE IF NOT EXISTS Task (id INTEGER NOT NULL IDENTITY, checked BOOL NULL, description VARCHAR(100) NULL)"
         )
     }
 
     @GetMapping("/tasks")
     ArrayList findAll() {
         def list = [] as ArrayList
-        def rows = statement.selectReturns "SELECT * FROM Task"
+        def rows = statement.selectReturns("SELECT * FROM Task")
         for (r in rows) {
-            list << new Task(id: r.id, checked: r.checked, description: r.description)
+            list.add(new Task(id: r.id, checked: r.checked, description: r.description))
         }
         return list
+    }
+
+    @PostMapping("/tasks")
+    void insert(@RequestBody Task task) {
+        try {
+            statement.executeWithTransaction(
+                "INSERT INTO Task (checked, description) VALUES (${task.checked}, ${task.description})"
+            )
+        } catch (Exception e) {
+            Logger.getLogger(this.class).severe(e.getMessage())
+        }
     }
 
 }
--- src/main/resources/static/js/task/TaskController.js
+++ src/main/resources/static/js/task/TaskController.js
@@ -1,8 +1,12 @@
 (function () {
     angular.module('app')
         .controller('TaskController', function($scope, $http) {
-            $scope.tasks = [];
-
+            $scope.tasks = [{id: 1, checked: false, description: 'teste', steps: [{id: 1, checked: false, description: 'teste'}]}];
+            var nextId = $scope.tasks.length + 1;
+            $scope.tasksNew = [
+                {id: nextId, icon: 'add', checked: false, description: undefined}
+            ];
+            $scope.task = {};
             function create() {
                 $http.get('/ng-md-todo/create').then(
                     function(response) {
@@ -14,16 +18,30 @@
                 );
             }
             create();
-            $scope.list = function () {
+            function list() {
                 $http.get('/ng-md-todo/tasks').then(
                     function(response) {
                         console.log('Dados retornados', response.data);
-                        $scope.tasks = response.data;
+                        //$scope.tasks = response.data;
                     },
                     function(error) {
                         console.log(error);
                     }
                 );
+            }
+            list();
+            $scope.reset = function (task) {
+                task.icon = '';
+                task.description = undefined;
+                window.setTimeout(() => {
+                    document.getElementById('txtDescription').focus();
+                }, 500);
+            };
+            $scope.insert = function (task) {
+                $scope.tasks.push({id: task.id, checked: task.checked, description: task.description, steps: []});
+                nextId = $scope.tasks.length + 1;
+                $scope.tasksNew = [];
+                $scope.tasksNew.push({id: nextId, icon: 'add', checked: false, description: undefined});
             };
         });
 })();
